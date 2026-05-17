7d61c1f9e70dab74da13a3259e8e3ad50b4472b3
Kamran Eisenberg <kamraneisenberg@mac.mynetworksettings.com>
2026-05-17 08:08:42 -0400

    Update infrastructure experience text

--- app.jsx
+++ app.jsx
@@ -844,7 +844,7 @@ function PortfolioHome({ setView }) {
           <article>
             <span>Experience</span>
             <strong>AI Infrastructure Intern</strong>
-            <p>SolutionsInfra<br />at Fort LP Hedge Fund</p>
+            <p>SolutionsInfra<br />IT @ Fort LP Hedge Fund</p>
           </article>
         </div>
       </section>
